fbb5d4f8ce5e71f1284f048f7c6ed254433a89e9
RTsoli <robotort2140@gmail.com>
2023-11-15 13:33:15 +0500

    my version add car

---
+++ src/main/java/ru/oop/Car.java
@@ -0,0 +1,14 @@
+package ru.oop;
+
+public class Car {
+    public Car(Position currentPosition) {
+        //TODO
+    }
+
+    /**
+     * Доехать маскимально близко до destination
+     */
+    public void go(Position destination) {
+        //TODO
+    }
+}
